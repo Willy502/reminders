bc755f6196aa753bdb8e342c9fa7d8d445e970d7
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-06-05 11:04:56 -0600

    DB Provider para creación, eliminación y recuperación de datos. Y creación de base de datos

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -2,15 +2,23 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:reminders/src/models/reminder_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
 
   // Singleton
-  static Database? database;
+  static Database? _database;
   static final DBProvider db = DBProvider._();
 
   DBProvider._();
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+
+    _database = await initDB();
+    return _database;
+  }
 
   initDB() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -29,6 +37,32 @@ class DBProvider {
           ')'
         );
     }); // LA VERSION SE DEBE INCREMENTARA CON CADA CAMBIO PARA IMPACTAR EN EL ARCHIVO DE BASE DE DATOS
+  }
+
+  // CREAR REGISTROS
+  Future<int?> newReminderRaw(ReminderModel reminder) async {
+    final db = await database;
+    final res = await db?.insert('Reminders', reminder.toJson());
+    return res;
+  }
+
+  // ELIMINAR REGISTROS
+  Future<int?> deleteReminder(int id) async {
+    final db = await database;
+    final res = await db?.delete('Reminders', where: 'id = ?', whereArgs: [id]);
+    return res;
+  }
+
+  // RECUPERAR REGISTROS
+  Future<List<ReminderModel>?> getReminders() async {
+    final db = await database;
+    final res = await db?.query('Reminders');
+
+    List<ReminderModel>? reminders = res!.isNotEmpty
+        ? res.map((reminder) => ReminderModel.fromJson(reminder)).toList()
+        : [];
+
+    return reminders;
   }
 
 }
